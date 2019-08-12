fc83b6305627abeb91ff4ffe911326ec987bc05d
Jonathan Lemman <jonathan.lemman@gmail.com>
2019-08-12 16:57:23 -0500

    finished exercise 4 on for_loops.js

---
+++ js/for_loops.js
@@ -0,0 +1,36 @@
+"use strict";
+
+
+
+
+function showMultiplicationTable(num) {
+    for(var i = 1; i <= 10; i++) {
+        var multi = num * i;
+        console.log(num + " x " + i + " = " + multi);
+    }
+}
+
+showMultiplicationTable(7);
+
+
+function tenRandomOddorEven() {
+    for(var i = 1; i <= 10; i++) {
+        var random = Math.floor(Math.random() * 200) + 20;
+        if(random % 2 === 0) {
+            console.log(random + " is even!");
+        } else {
+            console.log(random + " is odd!");
+        }
+    }
+}
+
+tenRandomOddorEven();
+
+
+function numPyramid() {
+    for(var i = 1; i <= 9; i++) {
+        console.log(i.toString().repeat(i));
+    }
+}
+
+numPyramid();
