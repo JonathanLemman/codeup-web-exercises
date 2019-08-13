3a62609ea142e46cf899b8f8f0004657f082be13
Jonathan Lemman <jonathan.lemman@gmail.com>
2019-08-13 10:13:46 -0500

    finished break_and_continue.js

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,1 +1,22 @@
 "use strict";
+
+
+// var oddUser = prompt("Enter an odd number");
+
+while(true) {
+    var oddUser = prompt("Enter an odd number");
+    if(oddUser % 2 === 1) {
+        break;
+    }
+}
+
+for(var i = 1; i <= 50; i++) {
+    if(i % 2 === 0) {
+        continue;
+    }
+    if(i === parseFloat(oddUser)){
+        console.log("Yikes! Skipping number: " + oddUser);
+        continue;
+    }
+    console.log("Here is an odd number: " + i);
+}
