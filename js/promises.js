7ab633bdc6446624d22533b557065c898885c4f1
Jonathan Lemman <jonathan.lemman@gmail.com>
2019-09-10 11:04:37 -0500

    finished first promise exercise

---
+++ js/promises.js
@@ -0,0 +1,23 @@
+"use strict";
+
+// ES5
+// function wait(num) {
+//     return new Promise(function (res, rej) {
+//         setTimeout(function () {
+//             res();
+//         }, num)
+//     });
+// }
+
+// ES6
+const wait = num => new Promise((res, rej) => setTimeout(() => res(), num));
+
+
+wait(1000).then(() => console.log('You\'ll see this after 1 second'));
+wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
+
+
+
+// 33e683853afe16decc1e781aba86975d4a64c591
+
+// fetch(url, {headers: {'Authorization': '33e683853afe16decc1e781aba86975d4a64c591'}});
